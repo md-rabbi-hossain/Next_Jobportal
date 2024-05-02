3551c74e9bb24b2b7bb1d0485e49ca538a26cb1e
Md Rabbi Hossain <mdrabbihossain1443@gmail.com>
2024-05-02 22:51:56 +0600

    Jobcard_update

--- app/Component/Helper/Jobcard.tsx
+++ app/Component/Helper/Jobcard.tsx
@@ -3,9 +3,13 @@ interface props {
   Job: Job;
 }
 const Jobcard = ({ Job }: props) => {
-  console.log(Job);
   return (
-    <div className="p-4 mb-6 relative border-2 cursor-pointer hover:scale-110 hover:shadow-sm transition-all duration-300  border-gray-500 rounded-lg opacity-5 "></div>
+    <div className="p-4 mb-6 border-2 cursor-pointer hover:scale-110 hover:shadow-sm transition-all duration-300 border-gray-500 rounded-lg h-[200px] overflow-hidden">
+      <h1 className="text-black font-semibold">{Job.title}</h1>
+      <p className="text-gray-700 overflow-hidden   h-24 overflow-ellipsis">
+        {Job.description}
+      </p>
+    </div>
   );
 };
 
