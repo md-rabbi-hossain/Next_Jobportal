a86490e68f844ed60be991e100a8cc34857bcbad
Md Rabbi Hossain <mdrabbihossain1443@gmail.com>
2024-05-18 22:41:14 +0600

    Feture job start

---
+++ app/Component/FeatureJob/FeatureJob.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Heading from "../Helper/Heading";
+
+const FeatureJob = () => {
+  return (
+    <div>
+      <Heading
+        Heading="Featured jobs"
+        subheading="Know your worth and find the job and qualify your life"
+      />
+    </div>
+  );
+};
+
+export default FeatureJob;
--- app/Component/Home/Home.tsx
+++ app/Component/Home/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Nav from "./Nav";
 import Hero from "./Hero";
 import Catagory from "../category/Catagory";
+import FeatureJob from "../FeatureJob/FeatureJob";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Catagory />
+      <FeatureJob />
     </div>
   );
 };
