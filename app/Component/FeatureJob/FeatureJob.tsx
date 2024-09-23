6ce0ec6f660cb151cde7098f647c51f191e66cd6
Md Rabbi Hossain <mdrabbihossain1443@gmail.com>
2024-09-23 13:42:12 +0600

    changes

--- app/Component/FeatureJob/FeatureJob.tsx
+++ app/Component/FeatureJob/FeatureJob.tsx
@@ -5,7 +5,6 @@ import Heading from "../Helper/Heading";
 import Jobcard from "../Helper/Jobcard";
 import { AppContext } from "../Context/Provider";
 import Button from "../Helper/Button";
-import Link from "next/link";
 
 interface Job {
   id: string;
@@ -13,21 +12,25 @@ interface Job {
 }
 
 const FeatureJob: React.FC = () => {
-  const { jobData } = useContext(AppContext)
+  const { jobData } = useContext(AppContext) as unknown as { jobData: Job[] };
+
+  if (!jobData) return <p>Loading...</p>;
+
   return (
     <>
       <Heading
         Heading="Featured jobs"
         subheading="Know your worth and find the job and qualify your life"
       />
-      <div className="mt-20 w-[90%] mx-auto  grid grid-cols-1 lg:grid-cols-3 gap-12 items-center ">
+      <div className="mt-20 w-[90%] mx-auto grid grid-cols-1 lg:grid-cols-3 gap-12 items-center">
         {jobData.map((item: Job) => (
           <Jobcard item={item.jobs} key={item.id} />
         ))}
       </div>
-      <Button text="View all job" url="/jobs" />
+      <Button text="View all jobs" url="/jobs" />
     </>
   );
 };
 
 export default FeatureJob;
+
